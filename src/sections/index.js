7dae13cc55a8e5d51a90320e7f26bfb0e88d2d33
ved0010 <122371158+ved279@users.noreply.github.com>
2024-06-20 22:15:05 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import Hero from './Hero';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+import PopularProducts from './PopularProducts';
+
+export{
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
